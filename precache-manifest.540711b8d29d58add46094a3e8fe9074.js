81abfb80120c247c48bf22c6653d2841b6041698
Michał Baumruck <embeproembe@gmail.com>
2020-01-21 12:21:24 +0100

    Updates

---
+++ precache-manifest.540711b8d29d58add46094a3e8fe9074.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee7c7aff22243481d7d3cb3e28dafd6",
+    "url": "/rr-app/index.html"
+  },
+  {
+    "revision": "ed64b917dfd638527f40",
+    "url": "/rr-app/static/css/main.faadd705.chunk.css"
+  },
+  {
+    "revision": "41c24f7fbfa143a35e57",
+    "url": "/rr-app/static/js/2.ec1576cc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/rr-app/static/js/2.ec1576cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed64b917dfd638527f40",
+    "url": "/rr-app/static/js/main.197810fa.chunk.js"
+  },
+  {
+    "revision": "8975fa3acdbb29ed9e39",
+    "url": "/rr-app/static/js/runtime-main.48a7bfe0.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/rr-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/rr-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/rr-app/static/media/header3.d9d36b65.jpg"
+  }
+]);
